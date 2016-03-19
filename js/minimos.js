24c71142782a209970d7af1c188b4eb1d607a688
Lizz <vane.cadavid013@gmail.com>
2016-03-19 12:40:16 -0500

    Pre merge

--- js/minimos.js
+++ js/minimos.js
@@ -1,10 +1,12 @@
 var n;
+var $valoresDOM = $('#inputs'),
+		$btnCalc = '<button type="button">Calcular minimos cuadrados</button>';
 
 $(document).ready(function(){
 	document.getElementById('formulario').onsubmit = function (event) {
 		event.preventDefault();
 		n = $('#NMuestra').val();
-		var $valoresDOM = $('#inputs');
+
 		$valoresDOM.append(
 			'<label>Entradas</label>'
 		);
@@ -14,10 +16,17 @@ $(document).ready(function(){
 			);
 		}
 
-		$valoresDOM.append('<button type="button">Calcular minimos cuadrados</button>')
+		$valoresDOM.append($btnCalc)
 	};
 
 	document.getElementById('Cancelar').onclick = function(){
 		location.href = "index.html";
 	};
+})
+
+//no existe aun por eso lo hago asi
+$('body').on($btnCalc, 'click', function(){
+	let data = {
+		x: [], y: []
+	}
 })
