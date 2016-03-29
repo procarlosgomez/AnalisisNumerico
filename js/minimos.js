0d6c8c8c1864d4f54d6333568bd43e2c0668b717
Dyeudiel <bucaro345@hotmail.com>
2016-03-28 21:57:40 -0500

    Minimos de functiones, arreglar la cosa que se me daño

--- js/minimos.js
+++ js/minimos.js
@@ -1,6 +1,8 @@
-var n;
+var n, m, b, minX, maxX, minY, maxY;
+
+var	sumX = sumY = sumXX = sumXY = 0;
 var $valoresDOM = $('#inputs'),
-		$btnCalc = '<button type="button">Calcular minimos cuadrados</button>',
+		$btnCalc = '<button type="button" id="btnCalc">Calcular minimos cuadrados</button>',
 		data = {
 			x: [], y: []
 		}
@@ -9,11 +11,13 @@ var $valoresDOM = $('#inputs'),
 $(document).ready(function(){
 	document.getElementById('formulario').onsubmit = function (event) {
 		event.preventDefault();
-		n = $('#NMuestra').val();
+		n = parseInt($('#NMuestra').val());
 
-		$valoresDOM.append(
-			'<label>Entradas</label>'
-		);
+		$valoresDOM
+			.empty()
+			.append(
+				'<label>Entradas</label>'
+			);
 		for(var i = 0; i<n; i++){
 			$valoresDOM.append(
 				'<div><input class="xVal" type="number" placeholder="x'+(i+1)+'"/><input class="yVal" type="number" placeholder="y'+(i+1)+'"/></div>'
@@ -28,15 +32,96 @@ $(document).ready(function(){
 	};
 })
 
+
 //no existe aun por eso lo hago asi
-$('body').on($btnCalc, 'click', function(){
+$('body').on('click', '#btnCalc', function(){
+	var data = {
+		x: [], y: []
+	};
+
+
+	var hasError = false;
 	$valoresDOM.find('input.xVal').each(function(){
-		data.xVal.push($(this).val());
+		if($(this).val() == ''){
+			hasError = true;
+			$(this).css('border', '1px solid red');
+		}
+
+		data.x.push(parseFloat($(this).val()));
 	})
 
 	$valoresDOM.find('input.yVal').each(function(){
-		data.yVal.push($(this).val());
+		if($(this).val() == ''){
+			hasError = true;
+			$(this).css('border', '1px solid red');
+		}
+
+		data.x.push(parseFloat($(this).val()));
 	})
 
-	console.log(data);
-})
+	if(hasError){
+		alert('Todas las Entradas son obligatorias');
+		return;
+	}
+
+	function sumData(){
+		sumX = sumY = sumXX = sumXY = 0;
+		for(var i in data.x){
+			sumX += data.x[i];
+			sumY += data.y[i];
+			sumXX += data.x[i]*data.x[i];
+			sumXY += (data.x[i] * data.y[i]);
+		}
+	}
+
+	function makeLineal(){
+		m = (((n*sumXY) - (sumX*sumY)) / ((n*sumXX)-(sumX*sumX)));
+		b = ((sumY - (m * sumX))/n);
+
+		var points = [];
+		for(i = 0; i<n; i++){
+			points.push([
+				data.x[i], data.y[i]
+			])
+		}
+
+		dataPlot = [{
+				fn: 				m+'x+'+b,
+				sampler:		'builtIn',  // this will make function-plot use the evaluator of math.js
+				graphType:	'polyline'
+			},
+			{
+				points: 		points,
+				fnType: 		'points',
+				graphType:	'scatter'
+			}
+		];
+
+		try {
+			var instance = functionPlot({
+				target: '#plotLineal',
+				data: dataPlot
+			});
+
+
+			var xDomain = [-10, 10]
+			var yDomain = [-1.897, 1.897]
+			instance.programmaticZoom(xDomain, yDomain)
+
+		}
+		catch (err) {
+			console.log(err);
+			alert(err);
+		}
+
+
+	}
+
+	sumData();
+	makeLineal();
+
+	console.log('-----');
+	console.log(sumX, sumY, sumXX, sumXY);
+	console.log(m, b);
+	//console.log(m, b);
+});
