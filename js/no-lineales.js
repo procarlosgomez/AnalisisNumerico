8120940f828a511c565c06a7747edd935399e79b
Dyeudiel <bucaro345@hotmail.com>
2016-04-27 16:09:09 -0500

    Newton-Raphson Se crea la funcion newtonRaphson. solo falta hacer que reciba los parametros por pantalla

--- js/no-lineales.js
+++ js/no-lineales.js
@@ -13,13 +13,13 @@ function puntoFijo(){
   do {
       comparador = resultado;
       //Convergente
-      //resultado = Math.pow((x + 3) / ((Math.pow(x, 2)) + 2), 0.5);
+      resultado = Math.pow((x + 3) / ((Math.pow(x, 2)) + 2), 0.5);
       //son iguales
       //resultado = Math.pow(((x+3-(Math.pow(x,4)))/2),0.5);
       //excede el limite por exponencial
-      resultado = Math.pow((x+2),2);
+      //resultado = Math.pow((x+2),2);
       //incremental
-      
+
       vect[contador] = resultado;
       x = resultado;
       contador += 1;
@@ -53,4 +53,49 @@ function puntoFijo(){
   //imprime el valor convergente y la interaccion
   console.log("\nInterpolacion: " + (resultado) + "\nPosicion: " + (contador - 1));
 }
+
+function newtonRaphson(){
+  var Pn = 3, resultado = 0, comparador;
+  var contador = 0;
+  var vect = [99999];
+  do {
+      comparador = resultado;
+      //Pn - (f(x)/f'(x));
+      //succesfull
+      resultado = Pn - ((Math.pow(Math.E, Pn-2)-Math.pow(Pn, 2)+7)/(Math.pow(Math.E, Pn-2)-(2*Pn)));
+      vect[contador] = resultado;
+      Pn = resultado;
+      contador += 1;
+      var Indefinido = comparador;
+      //Si la funcion tiene un error por superar el limite de caracteres
+      if (Indefinido==("Infinity")) {
+        console.log("No es Convergente, excede el limite por exponencial");
+        return;
+      }
+      //las interacciones de la fuincion son iguales eje [1,2,1,2,1,2,1,2]
+      if (contador >= 7) {
+          if (vect[contador - 1] == vect[contador - 3] & vect[contador - 1] == vect[contador - 5] &
+              vect[contador - 2] == vect[contador - 4] & vect[contador - 2] == vect[contador - 6]) {
+                console.log("No es Convergente, iguales");
+                return;
+          }
+          //si es incremental cierra el ciclo
+          if (vect[contador - 6] - vect[contador - 5] > (vect[contador - 5] - vect[contador - 4]) &
+              vect[contador - 5] - vect[contador - 4] > (vect[contador - 4] - vect[contador - 3]) &
+              vect[contador - 4] - vect[contador - 3] > (vect[contador - 3] - vect[contador - 2])) {
+                console.log("No es Convergente, incremental");
+                return;
+          }
+      }
+      //System.out.println("posicion: " + contador + " ans= " + resultado);
+  } while (comparador != resultado);
+  //imprime las interacciones cuando es convergente
+  for (var i = 0; i < contador - 1; i++) {
+    console.log("Posicion: " + (i + 1) + " Resultado: " + vect[i]);
+  }
+  //imprime el valor convergente y la interaccion
+  console.log("\nInterpolacion: " + (resultado) + "\nPosicion: " + (contador - 1));
+}
+
 puntoFijo();
+newtonRaphson();
